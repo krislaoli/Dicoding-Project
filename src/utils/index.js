d637e40fed8a558614b5200ed593f315ae1a2025
Pak Coding <laolikristian1305@gmail.com>
2023-11-04 20:58:03 +0700

    Up

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,46 +1,46 @@
 const getInitialData = () => ([
-  {
-    id: 1,
-    title: "Coding",
-    body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur minima cumque error odio accusamus mollitia et, ea quod perferendis porro!",
-    createdAt: '2023-10-04T04:27:34.572Z',
-    archived: false,
-  },
-  {
-    id: 2,
-    title: "React js",
-    body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur minima cumque error odio accusamus mollitia et, ea quod perferendis porro!",
-    createdAt: '2023-10-04T04:27:34.572Z',
-    archived: false,
-  },
-  {
-    id: 3,
-    title: "Belajar React",
-    body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur minima cumque error odio accusamus mollitia et, ea quod perferendis porro!",
-    createdAt: '2023-10-04T04:27:34.572Z',
-    archived: false,
-  },
-  {
-    id: 4,
-    title: "Fullstack",
-    body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur minima cumque error odio accusamus mollitia et, ea quod perferendis porro!",
-    createdAt: '2023-10-04T04:27:34.572Z',
-    archived: false,
-  },
-  {
-    id: 5,
-    title: "Front end",
-    body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur minima cumque error odio accusamus mollitia et, ea quod perferendis porro!",
-    createdAt: '2023-10-04T04:27:34.572Z',
-    archived: false,
-  },
-  {
-    id: 6,
-    title: "Back end",
-    body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur minima cumque error odio accusamus mollitia et, ea quod perferendis porro!",
-    createdAt: '2023-10-04T04:27:34.572Z',
-    archived: false,
-  },
+  // {
+  //   id: 1,
+  //   title: "Coding",
+  //   body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur minima cumque error odio accusamus mollitia et, ea quod perferendis porro!",
+  //   createdAt: '2023-10-04T04:27:34.572Z',
+  //   archived: false,
+  // },
+  // {
+  //   id: 2,
+  //   title: "React js",
+  //   body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur minima cumque error odio accusamus mollitia et, ea quod perferendis porro!",
+  //   createdAt: '2023-10-04T04:27:34.572Z',
+  //   archived: false,
+  // },
+  // {
+  //   id: 3,
+  //   title: "Belajar React",
+  //   body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur minima cumque error odio accusamus mollitia et, ea quod perferendis porro!",
+  //   createdAt: '2023-10-04T04:27:34.572Z',
+  //   archived: false,
+  // },
+  // {
+  //   id: 4,
+  //   title: "Fullstack",
+  //   body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur minima cumque error odio accusamus mollitia et, ea quod perferendis porro!",
+  //   createdAt: '2023-10-04T04:27:34.572Z',
+  //   archived: false,
+  // },
+  // {
+  //   id: 5,
+  //   title: "Front end",
+  //   body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur minima cumque error odio accusamus mollitia et, ea quod perferendis porro!",
+  //   createdAt: '2023-10-04T04:27:34.572Z',
+  //   archived: false,
+  // },
+  // {
+  //   id: 6,
+  //   title: "Back end",
+  //   body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur minima cumque error odio accusamus mollitia et, ea quod perferendis porro!",
+  //   createdAt: '2023-10-04T04:27:34.572Z',
+  //   archived: false,
+  // },
 ]);
 
 const showFormattedDate = (date) => {
